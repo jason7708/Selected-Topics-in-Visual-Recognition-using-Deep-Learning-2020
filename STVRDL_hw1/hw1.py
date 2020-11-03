44fc664090eb8e20ced0e0555ee319e985dfa21b
jason7708 <jason7708@gmail.com>
2020-11-03 14:46:20 +0800

    init

---
+++ STVRDL_hw1/hw1.py
@@ -0,0 +1,12 @@
+from torch.utils.data.dataset import Dataset
+
+class MyCustomDataset(Dataset):
+    def __init__(self, ...):
+        # stuff
+        
+    def __getitem__(self, index):
+        # stuff
+        return (img, label)
+
+    def __len__(self):
+        return count # of how many examples(images?) you have
